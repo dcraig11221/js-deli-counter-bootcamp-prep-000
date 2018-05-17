5bf26d1bcd0b0d49ef0a6685b4188650ae55d86b
Douglas Craig <dcraig112@gmail.com>
2018-05-17 19:25:46 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,26 +1,27 @@
 var katzDeli = []
+
 function takeANumber(array, person) {
   array.push(person)
   return `Welcome, ${person}. You are number ${array.length} in line.`
 }
 function nowServing(array) {
   if (array.length > 0) {
-    let current = array.shift()
-    return `Currently serving ${current}.`
+    let person = array.shift()
+    return `Currently serving ${person}.`
   }
   else {
-    return 'There is nobody waiting to be served!'
+    return `There is nobody waiting to be served!`
   }
 }
 function currentLine(line) {
-  let current = []
-  if (line.length > 0) {
-    for (var i = 0; i < line.length; i++) {
-      current.push(` ${i+1}. ${line[i]}`)
-    }
-    return `The line is currently:${current}`
+  if (line.length < 1) {
+    return `The line is currently empty.`
   }
   else {
-    return 'The line is currently empty.'
+    let newLine = []
+    for (let i = 0; i < line.length; i++) {
+      newLine.push(` ${i+1}. ${line[i]}`)
+    }
+    return `The line is currently:${newLine}`
   }
 }
