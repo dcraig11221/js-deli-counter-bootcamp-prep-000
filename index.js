51281106bc34b285b20bc7970b0d7c935ace1c8d
Douglas Craig <dcraig112@gmail.com>
2018-05-17 19:25:56 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,20 +4,20 @@ function takeANumber(array, person) {
   array.push(person)
   return `Welcome, ${person}. You are number ${array.length} in line.`
 }
+
 function nowServing(array) {
   if (array.length > 0) {
     let person = array.shift()
     return `Currently serving ${person}.`
-  }
-  else {
+  } else {
     return `There is nobody waiting to be served!`
   }
 }
+
 function currentLine(line) {
   if (line.length < 1) {
     return `The line is currently empty.`
-  }
-  else {
+  } else {
     let newLine = []
     for (let i = 0; i < line.length; i++) {
       newLine.push(` ${i+1}. ${line[i]}`)
